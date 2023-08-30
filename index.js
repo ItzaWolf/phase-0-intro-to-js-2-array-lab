30ae588e4dbb0f0142fe5a9825408f2c7ee95cc0
Kaleb Bolack <kalebbolack14@gmail.com>
2023-08-30 14:53:43 -0600

    Completed all test's!

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(Furballer) {
+    cats.push(Furballer);
+}
+
+function destructivelyPrependCat(Puss) {
+    cats.unshift(Puss);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(Kitty) {
+    return [...cats, Kitty]
+}
+
+function prependCat(Meow) {
+    return [Meow, ...cats]
+}
+
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
